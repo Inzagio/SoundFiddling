01daa74487d336ec8e93a326963e775d332f12af
inzagio <inzagio@gmail.com>
2019-12-07 16:49:21 +0100

    InitialCommit

---
+++ src/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using NAudio.Wave;
+
+namespace FfmpegTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var test = new SoundControls();
+            test.PlaySound();
+        }
+
+        
+    }
+}
---
+++ src/SoundControls.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using NAudio.Wave;
+
+namespace FfmpegTest
+{
+    class SoundControls
+    {
+        public void PlaySound()
+        {
+            var path = Directory.GetCurrentDirectory();
+            var file = Path.Combine(path, "AccessDenied.mp3");
+            using (var audioFile = new AudioFileReader(file))
+            using (var outputDevice = new WaveOutEvent())
+            {
+                outputDevice.Init(audioFile);
+                outputDevice.Play();
+                while (outputDevice.PlaybackState == PlaybackState.Playing)
+                {
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
